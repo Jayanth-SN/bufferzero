30587db01ff7c5c8e9d1d1c7f0f0d2398c5c2a41
Jayanth-SN <96295036+Jayanth-SN@users.noreply.github.com>
2022-02-02 17:00:31 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,70 @@
+var name="jayanth";
+var age=23;
+console.log(typeof name);
+console.log(typeof age);
+
+
+var a=1;
+var b="hello";
+var c=a+b;
+console.log(c);
+
+var a=0.1;
+var b=0.2;
+var c=a+b;
+console.log(c);
+
+var a=5;
+var a=10;
+console.log(a);
+
+var a,b;
+a=5;
+a=10;
+b=5;
+console.log(a);
+console.log(b);
+
+console.log(a);
+var a=10;
+
+var a;
+console.log(a);
+a=30;
+
+// console.log(r); r is not defined
+
+var d;
+console.log(d);
+
+// console.log(l); l is not defined
+l="hello";
+console.log(l);
+
+// var data=20;
+// (function(z){
+//     var x=10;
+//     console.log(x+z);
+// }[data]);
+
+// (function(c){
+//     var y=10;
+//     console.log(y+z);
+// }[data])
+
+
+let n=20;
+{
+    let p=10;
+    console.log(p+n);
+}
+{
+    const q=20;
+    console.log(q+n);
+}
+
+const firstname="jayanth";
+const lastname="sn"
+const fullname=`${firstname} ${lastname}`
+
+console.log(fullname);
